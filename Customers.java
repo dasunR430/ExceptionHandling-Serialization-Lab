9c47daa9d8b9f4469e645e26d241098ba448cb06
Vihanga Munasinghe <149761186+VihangaMunasinghe@users.noreply.github.com>
2024-03-20 17:22:48 +0530

    Updated CustomerNotFound exception.

--- Customers.java
+++ Customers.java
@@ -4,12 +4,17 @@ import java.util.List;
 public class Customers {
     List<Customer> customerList = new ArrayList<>();
 
-    public Customer getCustomer(String phoneNo){
+
+    private void setCustomers(){
+
+    }
+
+    public Customer getCustomer(String phoneNo) throws CustomerNotFound {
         for (Customer customer: customerList){
             if(customer.getPhoneNo().equals(phoneNo)){
                 return customer;
             }
         }
-
+        throw new CustomerNotFound();
     }
 }
