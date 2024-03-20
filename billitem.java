584fc6231ee0d33d708d707ad362daafbf13f0e8
UmeshaJayakody <umeshachamodjayakody@gmail.com>
2024-03-20 17:25:08 +0530

    create billitem class

---
+++ billitem.java
@@ -0,0 +1,36 @@
+public class billitem {
+    private String itemName;
+    private double itemprice;
+    private double discountprecentage;
+    private double quentity;
+    private double netprice;
+    private double discount;
+
+    public billitem(String itemName,double itemprice,double discountprecentage,double quentity){
+        this.itemName=itemName;
+        this.discountprecentage=discountprecentage;
+        this.quentity=quentity;
+        this.itemprice=itemprice;
+        calculate_netprice();
+        calculate_discount();
+    }
+    public void calculate_netprice(){
+        netprice=itemprice*quentity;
+    }
+    public void calculate_discount(){
+        discount=itemprice*(discountprecentage/100)*quentity;
+    }
+    public double getNetPrice(){
+        return netprice;
+    }
+    public double getDiscount(){
+        return discount;
+    }
+    public void print(){
+        System.out.println("Item Name : "+itemName+" | Item Price : "+itemprice+" | Quentity : "+quentity);
+        System.out.println("Net Price : "+(itemprice*quentity));
+        System.out.println("Discount  : "+(itemprice*(discountprecentage/100)*quentity));
+
+    }
+
+}
