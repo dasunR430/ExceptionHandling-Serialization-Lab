5b20294a341eaeacc1c4891a40131f870578d807
UmeshaJayakody <umeshachamodjayakody@gmail.com>
2024-03-20 18:01:07 +0530

    add Store,GrocertItem and IteamCodeNotFound classes

---
+++ Store.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    private static Store instance; //instance variable
+    public static Store getInstance(){
+        //checking is the instance is null abd creating if needed
+        if(instance == null){
+            instance = new Store();
+        }
+        return instance;
+    }//
+
+    List<GroceryItem> items = new ArrayList<>(); //List of items in the store
+
+    //Constructor of the store class
+    public Store(){
+        setItems();
+    }
+
+    //adding example items to the store
+    public void setItems(){
+        GroceryItem SignalToothPaste = new GroceryItem(1,256.00,"160g","01-02-2024","12-09-2025","Unilever",0);
+        GroceryItem Samaposha = new GroceryItem(2,565.00, "700g","11-03-2024","12-12-2024","CBL",0);
+        GroceryItem FreshMilk = new GroceryItem(3,550.00, "1l","01-02-2024","12-09-2025","Ambewela",0);
+        GroceryItem Kottume = new GroceryItem(4,130.00, "80g","01-02-2024","12-09-2025","Prima",0);
+        GroceryItem Whitesugar = new GroceryItem(5,42.00, "100g","01-02-2024","12-09-2025","Keells",0);
+        GroceryItem Dhal = new GroceryItem(6,48.00, "100g","01-02-2024","12-09-2025","Keells",0);
+        GroceryItem Sambarice = new GroceryItem(7,32.00, "100g","01-02-2024","12-09-2025","Araliya",0);
+        GroceryItem CocaCola = new GroceryItem(8,400.00, "1.5l","01-02-2024","12-09-2025","CocaCola",0);
+        items.add(SignalToothPaste);
+        items.add(Samaposha);
+        items.add(FreshMilk);
+        items.add(Kottume);
+        items.add(Whitesugar);
+        items.add(Dhal);
+        items.add(Sambarice);
+        items.add(CocaCola);
+    }
+
+    public GroceryItem getItem(int itemCode) throws ItemCodeNotFound{
+        for(GroceryItem item: items){
+            if(item.getItemCode() == itemCode){
+                return item;
+            }
+        }
+        throw new ItemCodeNotFound("Item not found!");
+    }
+
+}
